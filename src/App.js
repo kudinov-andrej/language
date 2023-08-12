df831b3302fe24c7338782ba64c7dc9a6fde3127
Andrej Kudinov <kudinov.andrej@list.ru>
2023-08-12 21:12:08 +0300

    сделал компонент Footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Case from './Case';
 import Result from './Result';
 import Form from './Form';
+import Footer from './Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Case />
       <Result />
       <Form />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,22 @@
+import './Footer.css';
+import React from 'react';
+import telegram from "./images/telegram.svg"
+import githubImg from "./images/github.svg"
+import vkImg from "./images/vk.svg"
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className='footer__logo'></div>
+            <nav className="footer-nav">
+                <ul className='footer__navigation'>
+                    <li className='footer__link'><a className='footer__link-punct' href="https://t.me/+79284702312"><img className='footer__link-img' src={telegram} target='blank' alt="Иконка телеграмм" /></a></li>
+                    <li className='footer__link'><a className='footer__link-punct' href="https://vk.com/andreika01"><img className='footer__link-img' src={vkImg} target='blank' alt="иконка Вконтакте" /></a></li>
+                    <li className='footer__link'><a className='footer__link-punct' href="https://github.com/kudinov-andrej" target='blank'><img className='footer__link-img' src={githubImg} alt="иконка Гитхаб" /></a></li>
+                </ul>
+            </nav >
+        </footer>
+    );
+}
+
+export default Footer;
